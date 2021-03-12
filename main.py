55f1930da12c2e3ab7394cdbd791252ed8d1697d
Dominik <Dominik@mbp>
2021-03-12 11:57:24 +0100

    Able to read rgb data into 2dimesional array

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from PIL import Image 
+import numpy as np
 
-file = "/Users/dominik/Documents/Projects/ASCII/beach.jpg"
+file = "/Users/dominik/Documents/Projects/Ascii/ASCII_ART/beach.jpg"
 
 im = Image.open(file)
 print("Height: ", im.height)
 print("Width:  ", im.width)
+
+# Load image into array
+imSequence = im.getdata()
+imArray = np.array(imSequence)
+
+print (imArray)
